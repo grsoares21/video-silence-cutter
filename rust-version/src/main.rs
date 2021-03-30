4098274e167274d12605af41cc5b7fe0879c0df1
Gabriel Restori Soares <grsoares21@gmail.com>
2021-03-30 16:05:26 +0200

    Started implementing rust version, already capable of splitting audio

---
+++ rust-version/src/main.rs
@@ -0,0 +1,72 @@
+use getopts::Options;
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+
+const TEMP_DIR: &str = "./temp";
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+
+    opts.optopt(
+        "i",
+        "input",
+        "Set input file",
+        "Indicate the video file to cut the sileces from",
+    );
+    opts.optopt(
+        "o",
+        "output",
+        "Set output file",
+        "Indicate the path of the output video file",
+    );
+
+    // in Rust the first argument is the program itself and the following ones are the real args
+    let matches = match opts.parse(&args[1..]) {
+        Ok(matches) => matches,
+        Err(error) => {
+            panic!(error.to_string())
+        }
+    };
+
+    let input_file_name = match matches.opt_str("i") {
+        Some(file_name) => file_name,
+        None => panic!("You need to pass an input file (-i option)"),
+    };
+    let output_file_name = match matches.opt_str("o") {
+        Some(file_name) => file_name,
+        None => panic!("You need to pass an output file (-o option)"),
+    };
+
+    if Path::new(TEMP_DIR).is_dir() {
+        println!(
+            "Found existing {} directory, deleting and recreating it",
+            TEMP_DIR
+        );
+        fs::remove_dir_all(TEMP_DIR)?;
+    }
+
+    fs::create_dir(TEMP_DIR)?;
+
+    println!(
+        "Input file name: {}, output file name: {}",
+        input_file_name, output_file_name
+    );
+
+    let mut split_audio_process = Command::new("ffmpeg")
+        .args(&["-i", &input_file_name])
+        .args(&["-ab", "160k"])
+        .args(&["-ac", "2"])
+        .args(&["-vn", "temp/audio.wav"])
+        .spawn()
+        .expect("Failed to spawn process to separate video from audio");
+
+    split_audio_process
+        .wait()
+        .expect("Failed to wait for process to separate video from audio");
+
+    Ok(())
+}
